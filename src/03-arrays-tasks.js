45e9f90253a6497a930def94300fb4d1afb1c43c
Kirill Gudeta <kirillgudeta@gmail.com>
2024-09-01 20:40:21 +0300

    done for today

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -203,8 +203,13 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  const x = arr.flat();
+  let result = [];
+  x.map((el, i) => {
+    if (i % 3 === 0 && i != 0) {
+    }
+  });
 }
 
 /**
